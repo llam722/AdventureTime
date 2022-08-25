9557c049b1fe4aa01a6a99a12813d4bc311762db
louislam <louislam7229@gmail.com>
2022-08-25 08:42:50 -0700

    small edit

--- src/background.js
+++ src/background.js
@@ -1,5 +1,12 @@
 // "default_popup": "index.html"
 
+
+//<script>
+// function myFunction(){
+//   document.getElementbyId('window').style.cursor = url('src/assets/cursor/pokeball.png'), auto;
+// }
+// </script>
+
 let color = "#3aa757";
 
 chrome.runtime.onInstalled.addListener(() => {
