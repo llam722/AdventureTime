3c4e0b1ba45988f91e24c4a3bdf736585a42b79c
louislam <louislam7229@gmail.com>
2022-08-25 08:30:46 -0700

    trying background changes

--- src/background.js
+++ src/background.js
@@ -1,1 +1,8 @@
 // "default_popup": "index.html"
+
+let color = "#3aa757";
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({ color });
+  console.log("Default background color set to %cgreen", `color: ${color}`);
+});
