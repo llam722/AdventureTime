9e130e970ca4f3b6daf88ef422da3d2a7a65d5f3
louislam <louislam7229@gmail.com>
2022-08-25 01:25:01 -0700

    tried to add cursor

--- src/main.js
+++ src/main.js
@@ -1,36 +1,33 @@
-
 // PLACE POKEMON ON PAGE:
 
-
-
 // select element by image tag
-const imgArr = document.getElementsByTagName('img')
+const imgArr = document.getElementsByTagName("img");
 
 // create random number
-let randomNum = Math.floor(Math.random() * (imgArr.length));
+let randomNum = Math.floor(Math.random() * imgArr.length);
 
 // create pokemon next to image selected by random number
 const selectedImg = imgArr[randomNum];
-console.log("about to create a pokemon")
+console.log("about to create a pokemon");
 new CreatePokemon(selectedImg);
-
 
 // Add board ontop of existing page -> add to DOM
 // Add polemon image (maybe a buttom) with event listener
 // When clicked, catch pokemon
 
+// document.body.style.cursor = "src/assets/cursor/pokemon.png";
 
+$("html").css("cursor: url('src/assets/cursor/pokemon.png'), auto");
+// let elementToChange = document.getElementsByTagName("body")[0];
+// elementToChange.style.cursor = "url('src/assets/cursor/pokemon.png'), auto";
+// class Cursor {
+//   constructor(el) {
+//     this.style.cursor = "crosshair";
+//   }
+// }
 
-
-
-
-
-
-class Cursor {
-  constructor(el){
-  this.style.cursor = 'crosshair'
-
-  }
-}
-
-const newCursor = new Cursor();
+// const newCursor = new Cursor();
+// (document.getElementById("demo").style.cursor = url(
+//   "src/assets/cursor/pokemon.png"
+// )),
+//   auto;
