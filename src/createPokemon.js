207395006b659765086dd2b82e738a42a887901d
sher85 <32082005+sher85@users.noreply.github.com>
2022-08-24 21:41:50 -0500

    folder structure update and general changes in the files

---
+++ src/createPokemon.js
@@ -0,0 +1,14 @@
+class createPokemon {
+    constructor(element) {
+        // Create image
+        this.node = document.createElement("img");
+        
+        // Set attributes
+        pokemon.setAttribute("id", "pokemon");
+        pokemon.setAttribute("src", "src/assets/pikachu1.png");
+        
+        // Append child node
+        element.appendChild(this.node)
+
+    }
+}
--- src/main.js
+++ src/main.js
@@ -7,11 +7,29 @@
 const imgArr = document.getElementsByTagName('img')
 
 // create random number
-let randomNum = Math.floor(Math.random() * (imgArr.length);
+let randomNum = Math.floor(Math.random() * (imgArr.length));
 
 // create pokemon next to image selected by random number
-const selectedImg = imgArr[randomNum]
+const selectedImg = imgArr[randomNum];
+new createPokemon(selectedImg);
+
 
 // Add board ontop of existing page -> add to DOM
 // Add polemon image (maybe a buttom) with event listener
 // When clicked, catch pokemon
+
+
+
+
+
+
+
+
+class Cursor {
+  constructor(el){
+  this.style.cursor = 'crosshair'
+
+  }
+}
+
+const newCursor = new Cursor();
