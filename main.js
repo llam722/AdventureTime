e73bce8a2c32ca420db6b81b0a39976c49547034
sher85 <32082005+sher85@users.noreply.github.com>
2022-08-24 21:12:35 -0500

    updates git push

--- main.js
+++ main.js
@@ -7,10 +7,10 @@
 const imgArr = document.getElementsByTagName('img')
 
 // create random number
-let randomNum = Math.floor(Math.random() * (imgArr.length - 0 + 1)) + 0;
+let randomNum = Math.floor(Math.random() * (imgArr.length);
 
 // create pokemon next to image selected by random number
-const selectedImg = imgArr[]
+const selectedImg = imgArr[randomNum]
 
 // Add board ontop of existing page -> add to DOM
 // Add polemon image (maybe a buttom) with event listener
