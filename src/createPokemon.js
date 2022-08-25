05d3ad80fecfe801a2efbb68a929205773a540ba
sher85 <32082005+sher85@users.noreply.github.com>
2022-08-24 21:53:39 -0500

    folder structure

--- src/createPokemon.js
+++ src/createPokemon.js
@@ -7,7 +7,7 @@ class createPokemon {
         pokemon.setAttribute("id", "pokemon");
         pokemon.setAttribute("src", "src/assets/pikachu1.png");
         
-        // Append child node
+        // Append as child node
         element.appendChild(this.node)
 
     }
